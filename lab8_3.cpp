d29843bd7ee8759e0e3eb239f501ed4a26d584c7
irxzleam <maelzxri@gmail.com>
2025-12-16 16:10:22 +0700

    3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
 /*
 "Enter your age: "
 "Enter your height: "
@@ -11,3 +15,44 @@
 "Brook"
 "Jinbe"
 */
+
+
+int main()
+{
+    int age,height,bounty;
+    string c;
+
+    cout << "Enter your age: ";
+    cin >> age;
+        if(age<=25){
+            cout << "Enter your height: ";
+            cin >> height;
+                if (height<100){
+                    c="Chopper";
+                }else{
+                    if (height<180){
+                        c="Usopp";
+                    }else{
+                        cout << "Enter your bounty: ";
+                        cin >> bounty;
+                            if(bounty>1100000000){
+                                c="Zoro";
+                            }else{
+                                c="Sanji";
+                            }
+                    }
+                }
+
+        }else if(age<=60){
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+                if (bounty>500000000){
+                    c="Jinbe";
+                }else{
+                    c="Franky";
+                }
+        }else{
+            c= "Brook";
+        }
+cout << "Your character = " << c;
+}
